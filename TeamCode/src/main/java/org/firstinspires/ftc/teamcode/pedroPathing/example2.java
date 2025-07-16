cdf866c587b8fa8b19096c5cf6b386835608b8cc
E <127677871+Great3github@users.noreply.github.com>
2025-07-16 13:02:32 -0400

    Create new auton
    trying to firgure out how my first auton worked but my second didn't

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/example2.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.PathBuilder;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+
+@Autonomous (name="Mattp2", group="Examples")
+public class example2 extends OpMode {
+    MattPath path = new MattPath();
+    Follower follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
+    @Override
+    public void init() {
+
+
+
+        follower.setStartingPose(new Pose(10, 60, Math.toRadians(0)).getAsFTCStandardCoordinates());
+        follower.followPath(path.followerPath(), true);
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+    }
+    public static class MattPath {
+
+        public PathChain followerPath() {
+            PathBuilder builder = new PathBuilder();
+
+            builder
+                    .addPath(
+                            // Line 1
+                            new BezierCurve(
+                                    new Point(10.000, 60.000, Point.CARTESIAN),
+                                    new Point(37.000, 24.000, Point.CARTESIAN),
+                                    new Point(84.000, 24.000, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 2
+                            new BezierCurve(
+                                    new Point(84.000, 24.000, Point.CARTESIAN),
+                                    new Point(143.000, 49.000, Point.CARTESIAN),
+                                    new Point(110.000, 71.913, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(180));
+
+            return builder.build();
+        }
+    }
+}
