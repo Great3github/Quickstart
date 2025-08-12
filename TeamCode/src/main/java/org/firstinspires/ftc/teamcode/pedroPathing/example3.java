4831680eac5a42a81fe946b75f8a55c59cf53d7d
E <127677871+Great3github@users.noreply.github.com>
2025-08-12 19:37:01 -0400

    thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/example3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/example3.java
@@ -4,10 +4,12 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
+import com.pedropathing.localization.constants.OTOSConstants;
 import com.pedropathing.pathgen.BezierCurve;
 import com.pedropathing.pathgen.PathBuilder;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -20,6 +22,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 @Autonomous (name="Mattp2-1", group="Examples")
 public class example3 extends OpMode {
     PathChain path;
+    SparkFunOTOS.Pose2D newpose = OTOSConstants.offset;
 //    HardwareMap hardwareMap;
     private PathChain builder;
     private Follower follower;
@@ -32,11 +35,12 @@ public class example3 extends OpMode {
         follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
         follower.setStartingPose(new Pose(12.5, 85, Math.toRadians(0)).getAsFTCStandardCoordinates());
         // ^^ remember to set starting pose ^^
-        path = new MattPath().GeneratedPath2();
+        path = new MattPath().GeneratedPath3();
         follower.followPath(path, 0.3, true);
 
         telemetryA = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
-
+        telemetryA.addData("XValue", newpose.x);
+        telemetryA.addData("YValue", newpose.y);
         telemetryA.update();
     }
     public void start() {
@@ -47,9 +51,11 @@ public class example3 extends OpMode {
         follower.update();
         follower.telemetryDebug(telemetryA);
         //follower.setTeleOpMovementVectors(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, false);
-        if (gamepad1.a) {
+
+        if (!follower.isBusy()) {
             servo.setPower(-1);
-        } else if (gamepad1.b) {
+
+        } else if (gamepad1.b && follower.atParametricEnd()) {
             servo.setPower(1);
         } else {
             servo.setPower(0);
@@ -102,5 +108,49 @@ public class example3 extends OpMode {
             
             return builder;
         }
+        public PathChain GeneratedPath3() {
+            builder = follower.pathBuilder()
+
+
+                    .addPath(
+                            // Line 1
+                            new BezierCurve(
+                                    new Point(new Pose(9.757, 84.983, 0).getAsFTCStandardCoordinates()),
+                                    new Point(new Pose(9.863, 19.009, 0).getAsFTCStandardCoordinates()),
+                                    new Point(new Pose(76.214, 13.270, 0).getAsFTCStandardCoordinates())
+                            )
+                    )
+                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .addPath(
+                            // Line 2
+                            new BezierCurve(
+                                    new Point(new Pose(76.214, 13.270, 0).getAsFTCStandardCoordinates()),
+                                    new Point(new Pose(143.283, 13.629, 0).getAsFTCStandardCoordinates()),
+                                    new Point(new Pose(119.791, 75.318, 0).getAsFTCStandardCoordinates())
+                            )
+                    )
+                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .addPath(
+                            // Line 3
+                            new BezierCurve(
+                                    new Point(new Pose(119.791, 75.318, 0).getAsFTCStandardCoordinates()),
+                                    new Point(new Pose(138.441, 132.702, 0).getAsFTCStandardCoordinates()),
+                                    new Point(new Pose(72.807, 126.964, 0).getAsFTCStandardCoordinates())
+                            )
+                    )
+                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .addPath(
+                            // Line 4
+                            new BezierCurve(
+                                    new Point(new Pose(72.807, 126.964, 0).getAsFTCStandardCoordinates()),
+                                    new Point(new Pose(10.042, 134.496, 0).getAsFTCStandardCoordinates()),
+                                    new Point(new Pose(12.912, 88.767, 0).getAsFTCStandardCoordinates())
+                            )
+                    )
+                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .build();
+            return builder;
+        }
+
     }
 }
